91e9b6086b7f0f8818878c3e1015eecdecc3c682
damaresresende <dc.oliveiraresende@gmail.com>
2019-03-26 07:07:36 -0300

    Encoder for VS, VIS and SEM features

--- encoder.py
+++ encoder.py
@@ -11,7 +11,9 @@ Model to encode visual and semantic features of images
 '''
 
 import os
+import gc
 import tensorflow as tf
+from keras import backend as K
 from keras.utils import normalize
 from sklearn.model_selection import train_test_split
 from keras.backend.tensorflow_backend import set_session
@@ -20,22 +22,23 @@ from core.featuresparser import FeaturesParser
 from core.annotationsparser import PredicateType
 from core.vsautoencoder import VSAutoencoder
 
+def clear_memmory():
+    '''
+    Resets Tensorflow graph, clear Keras session and calls garbage collector
+    '''
+    tf.reset_default_graph()
+    K.clear_session()
+    gc.collect()
 
-def main():
+def run_encoder(X, Y, res_path):
+    '''
+    Runs autoencoder and plots results. It automatically splits the data set into 
+    training and test sets
     
-    fls_path = os.path.join(os.getcwd(), '_files/awa2')
-    fts_path = os.path.join(fls_path, 'features/ResNet101')
-    res_path = os.path.join(fls_path, 'results')
-    ann_path = os.path.join(fls_path, 'base')
-    
-    parser = FeaturesParser(fts_path)
-    vis_fts = parser.get_visual_features()
-    sem_fts = normalize(parser.get_semantic_features(ann_path, 
-                                                     PredicateType.CONTINUOUS) + 1, 
-                                                     order=1, axis=1)
-    
-    Y = parser.get_labels()
-    X = parser.concatenate_features(vis_fts, sem_fts)
+    @param X: data set
+    @param Y: labels set
+    @param res_path: results path to save results under
+    '''
     x_train, x_test, _, y_test = train_test_split(X, Y, stratify=Y, test_size=0.2)
     
     ae = VSAutoencoder()
@@ -49,9 +52,29 @@ def main():
     ae.plot_pca_vs_encoding(x_test, encoded_fts, os.path.join(res_path, 'ae_components.png'))
     ae.plot_spatial_distribution(x_test, encoded_fts, decoded_fts, y_test, 
                                  os.path.join(res_path, 'ae_distribution.png'))
+    clear_memmory()
+
+def main():
+    fls_path = os.path.join(os.getcwd(), '_files/awa2')
+    fts_path = os.path.join(fls_path, 'features/ResNet101')
+    res_path = os.path.join(fls_path, 'results')
+    ann_path = os.path.join(fls_path, 'base')
+    
+    if not os.path.isdir(res_path):
+        os.mkdir(res_path)
+                
+    parser = FeaturesParser(fts_path)
+    vis_fts = parser.get_visual_features()
+    sem_fts = normalize(parser.get_semantic_features(ann_path, 
+                                                     PredicateType.CONTINUOUS) + 1, 
+                                                     order=1, axis=1)
+    
+    Y = parser.get_labels()
+    run_encoder(vis_fts, Y, os.path.join(res_path, 'vis'))
+    run_encoder(sem_fts, Y, os.path.join(res_path, 'sem'))
+    run_encoder(parser.concatenate_features(vis_fts, sem_fts), Y, os.path.join(res_path, 'con'))
 
 if __name__ == '__main__':
-    
     config = tf.ConfigProto(log_device_placement=True)
     config.gpu_options.per_process_gpu_memory_fraction = 0.3
     set_session(tf.Session(config=config))
