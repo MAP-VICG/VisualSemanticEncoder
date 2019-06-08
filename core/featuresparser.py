c07f9b83ff0bb42fdefe3cb4af121d7e51ea398e
damaresresende <dc.oliveiraresende@gmail.com>
2019-06-08 19:46:54 -0300

    Refactoring features parser

--- core/featuresparser.py
+++ core/featuresparser.py
@@ -10,24 +10,35 @@ Retrieves features of 37322 images extracted with ResNet101. Each feature vector
     Institute of Mathematics and Computer Science (ICMC) 
     Laboratory of Visualization, Imaging and Computer Graphics (VICG)
 '''
-
+import os
 import numpy as np
-from os.path import join
 from keras.utils import normalize
 
 from utils.logwriter import Logger, MessageType
-from core.annotationsparser import AnnotationsParser, PredicateType
+from core.annotationsparser import AnnotationsParser
 
 
 class FeaturesParser():
     
-    def __init__(self, features_path):
+    def __init__(self, fts_dir=None, console=False):
         '''
         Initialization
         
-        @param base_path: string that points to path where the features data files are
+        @param fts_dir: string that points to folder where the features data files are
+        @param console: if True, prints debug in console
         '''
-        self.features_path = features_path
+        if fts_dir and isinstance(fts_dir, str):
+            base_path = os.path.join(os.path.join(os.path.join(os.getcwd().split('SemanticEncoder')[0], 
+                                                           'SemanticEncoder'), '_files'), fts_dir)
+        else:
+            base_path = os.path.join(os.path.join(os.path.join(os.getcwd().split('SemanticEncoder')[0], 
+                                                           'SemanticEncoder'), '_files'), 'features')
+        
+        self.features = os.path.join(base_path, 'AwA2-features.txt')
+        self.filenames = os.path.join(base_path, 'AwA2-filenames.txt')
+        self.labels = os.path.join(base_path, 'AwA2-labels.txt')
+        self.logger = Logger(console=console)
+        self.console = console
         
     def get_labels(self):
         '''
@@ -36,8 +47,7 @@ class FeaturesParser():
         @return numpy array of integers with labels
         '''
         try:
-            file_path = join(self.features_path, 'AwA2-labels.txt')
-            with open(file_path) as f:
+            with open(self.labels) as f:
                 lines = f.readlines()
                 labels = np.zeros((len(lines),), dtype=np.int32)
                 
@@ -46,7 +56,7 @@ class FeaturesParser():
                 
             return labels
         except FileNotFoundError:
-            Logger().write_message('File %s could not be found.' % file_path, MessageType.ERR)
+            self.logger.write_message('File %s could not be found.' % self.labels, MessageType.ERR)
             return None
         
     def get_visual_features(self, norm=False, norm_axis=1):
@@ -57,8 +67,7 @@ class FeaturesParser():
         @return numpy array of shape (37322, 2048) with features for images in AwA2 data set
         '''
         try:
-            file_path = join(self.features_path, 'AwA2-features.txt')
-            with open(file_path) as f:
+            with open(self.features) as f:
                 lines = f.readlines()
                 features = np.zeros((len(lines), 2048), dtype=np.float32)
                 
@@ -67,60 +76,41 @@ class FeaturesParser():
                         features[i, j] = float(value)
                 
             if norm:
-                Logger().write_message('Normalizing visual features.', MessageType.INF)
+                self.logger.write_message('Normalizing visual features.', MessageType.INF)
                 return normalize(features, order=2, axis=norm_axis)
         
             return features
         except FileNotFoundError:
-            Logger().write_message('File %s could not be found.' % file_path, MessageType.ERR)
+            self.logger.write_message('File %s could not be found.' % self.features, MessageType.ERR)
             return None
     
-    def get_semantic_features(self, ann_path, ptype=PredicateType.BINARY, subset=False, norm=False, norm_axis=1):
+    def get_semantic_features(self, subset=False, norm=False, norm_axis=1):
         '''
         Retrieves semantic features based on annotations
         
-        @param ann_path: annotation path
-        @param ptype: predicate type (binary or continuous)
         @param norm: normalize features
         @param subset: if True return a subset of the features with 19 attributes only
-        @return numpy array of shape (37322, 85) with features for images in AwA2 data set
+        @return numpy array of shape (37322, X) with features for images in AwA2 data set where X
+                is the number of attributes considered
         '''
-        ann_parser = AnnotationsParser(ann_path)
+        ann_parser = AnnotationsParser(self.console)
         
         if subset:
-            Logger().write_message('Using a subset of the semantic features', MessageType.INF)
-            att_map = ann_parser.get_subset_features(ann_parser.get_subset_annotations(), ptype)
+            self.logger.write_message('Using a subset of the semantic features', MessageType.INF)
+            att_map = ann_parser.get_predicate_matrix(subset=True)
         else:
-            Logger().write_message('Using whole set of the semantic features', MessageType.INF)
-            att_map = ann_parser.get_attributes(ptype)
+            self.logger.write_message('Using whole set of the semantic features', MessageType.INF)
+            att_map = ann_parser.get_predicate_matrix()
         
-        labels = ann_parser.get_labels()
-        available_labels = self.get_labels()
-        features = np.zeros((available_labels.shape[0], att_map.shape[1]), dtype=np.float32)
+        classes = ann_parser.get_classes()
+        labels = self.get_labels()
+        features = np.zeros((labels.shape[0], att_map.shape[1]), dtype=np.float32)
         
-        for idx, label in enumerate(available_labels):
-            features[idx, :] = att_map.loc[labels[label-1]].values
+        for idx, label in enumerate(labels):
+            features[idx, :] = att_map.loc[classes[label-1]].values
         
         if norm:
-            Logger().write_message('Normalizing semantic features.', MessageType.INF)
+            self.logger.write_message('Normalizing semantic features.', MessageType.INF)
             return normalize(features, order=2, axis=norm_axis)
         
-        return features
-    
-    @staticmethod
-    def concatenate_features(vis_fts, sem_fts, nfts):
-        '''
-        Concatenates semantic and visual features along x axis
-        
-        @param vis_fts: visual features
-        @param sem_fts: semantic features
-        @param nfts: number of semantic features
-        @return: numpy array of shape (37322, 2048 + 85) with all features
-        '''
-        features = np.zeros((vis_fts.shape[0], 2048 + nfts), dtype=np.float32)
-         
-        for ft in range(features.shape[0]):
-            features[ft,:vis_fts.shape[1]] = vis_fts[ft]
-            features[ft,vis_fts.shape[1]:] = sem_fts[ft] 
-  
         return features
--- test/test_featuresparser.py
+++ test/test_featuresparser.py
@@ -9,75 +9,57 @@ Tests for module featuresparser
     Institute of Mathematics and Computer Science (ICMC) 
     Laboratory of Visualization, Imaging and Computer Graphics (VICG)
 '''
-
+import os
 import unittest
 import numpy as np
 
 from core.featuresparser import FeaturesParser
 
 
-class AnnotationsParserTests(unittest.TestCase):
+class FeaturesParserTests(unittest.TestCase):
+        
+    @classmethod
+    def setUpClass(cls):
+        '''
+        Initializes model for all tests
+        '''
+        cls.parser = FeaturesParser(fts_dir=os.path.join('features', 'mock'), console=True)
         
     def test_get_labels(self):
         '''
         Tests if array of labels is retrieved
         '''
-        parser = FeaturesParser('./_mockfiles/awa2/features/ResNet101')
-        labels = parser.get_labels()
+        labels = self.parser.get_labels()
          
         self.assertTrue(isinstance(labels, np.ndarray))
         self.assertEqual((1000,), labels.shape)
         for label in labels:
             self.assertTrue(label <= 50 and label >= 1)
              
-    def test_get_labels_file_not_found(self):
-        '''
-        Tests if None is returned when file is not found
-        '''
-        parser = FeaturesParser('./_mockfiles/awa2/features/dummy/')
-        labels = parser.get_labels()
-         
-        self.assertEqual(None, labels)
-         
     def test_get_viual_features(self):
         '''
         Tests if array of features is retrieved
         '''
-        parser = FeaturesParser('./_mockfiles/awa2/features/ResNet101')
-        features = parser.get_visual_features()
-          
+        features = self.parser.get_visual_features()
+           
         self.assertEqual((1000, 2048), features.shape)
         self.assertTrue(sum(sum(features)) != 0)
         self.assertTrue(sum(sum(features)) != 1)
-          
-    def test_get_features_file_not_found(self):
-        '''
-        Tests if None is returned when file is not found
-        '''
-        parser = FeaturesParser('./_mockfiles/awa2/features/dummy/')
-        features = parser.get_visual_features()
-          
-        self.assertEqual(None, features)
-          
+
     def test_get_semantic_features(self):
         '''
         Tests if semantic features are correctly retrieved
         '''
-        parser = FeaturesParser('./_mockfiles/awa2/features/ResNet101')
-        features = parser.get_semantic_features('./_mockfiles/awa2/base/')
-          
+        features = self.parser.get_semantic_features()
+           
         self.assertEqual((1000, 85), features.shape)
         self.assertTrue(sum(sum(features)) > 0)
-         
-    def test_concatenate_features(self):
-        '''
-        Tests if features are correctly concatenated
-        '''
-        parser = FeaturesParser('./_mockfiles/awa2/features/ResNet101')
-        vis_fts = parser.get_visual_features()
-        sem_fts = parser.get_semantic_features('./_mockfiles/awa2/base/')
-        features = parser.concatenate_features(vis_fts, sem_fts)
-         
-        self.assertEqual((1000, 2048 + 85), features.shape)
-        self.assertTrue(sum(sum(features)) > 0)
         
+    def test_get_semantic_features_subset(self):
+        '''
+        Tests if semantic features are correctly retrieved
+        '''
+        features = self.parser.get_semantic_features(subset=True)
+           
+        self.assertEqual((1000, 23), features.shape)
+        self.assertTrue(sum(sum(features)) > 0)
