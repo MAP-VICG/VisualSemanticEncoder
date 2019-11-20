f4b377ce527c00456bd5f2784014a5d95ecfb4a3
Damares Resende <dc.oliveiraresende@gmail.com>
2019-11-20 08:17:11 -0200

    Explicitly setting features path for features parser

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ def main():
     config.read_configuration()
     
     log = LogWritter(logpath=config.results_path, console=config.console)
-    parser = FeaturesParser(config.features_path)
+    parser = FeaturesParser(fts_dir=config.features_path)
         
     log.write_message('Noise rate %s' % str(config.noise_rate), MessageType.INF)
     
