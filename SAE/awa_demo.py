2764b7c244e157e6c6e15cafd2702f33068b6708
damaresende <dc.oliveiraresende@gmail.com>
2020-03-30 00:10:12 -0300

    Adding SAE code. Kodirov et al

---
+++ SAE/awa_demo.py
@@ -0,0 +1,38 @@
+"""
+Following code shows a demo for aPY dataset to reproduce the result of the paper:
+
+Semantic Autoencoder for Zero-shot Learning.
+
+Elyor Kodirov, Tao Xiang, and Shaogang Gong, CVPR 2017.
+
+Code originally written in Matlab and transformed to Python by Damares Resende
+"""
+import numpy as np
+from scipy.io import loadmat
+from utils import ZSL, Params
+
+awa = loadmat('../../Datasets/SAE/awa_demo_data.mat')
+
+X_tr = np.array(awa['X_tr'])
+S_tr = np.array(awa['S_tr'])
+X_te = np.array(awa['X_te'])
+S_te_gt = np.array(awa['S_te_gt'])
+S_te_pro = np.array(awa['S_te_pro'])
+
+X_tr = ZSL.normalize_fea(X_tr.transpose()).transpose()
+
+lambda_ = 500000
+W = ZSL.SAE(X_tr.transpose(), S_tr.transpose(), lambda_)
+param = Params(1, awa, 'awa')
+
+# [F --> S], projecting data from feature space to semantic space
+S_est = X_te.dot(ZSL.normalize_fea(W).transpose())
+zsl_accuracy, _ = ZSL.zsl_el(S_est, S_te_gt, param, 'awa')
+
+print('\n[1] AwA ZSL accuracy [V >>> S]: %.1f%%\n' % (zsl_accuracy * 100))
+
+# [S --> F], projecting from semantic to visual space
+X_te_pro = ZSL.normalize_fea(S_te_pro.transpose()).transpose().dot(ZSL.normalize_fea(W))
+zsl_accuracy, _ = ZSL.zsl_el(X_te, X_te_pro, param, 'awa')
+
+print('[2] AwA ZSL accuracy [S >>> V]: %.1f%%\n' % (zsl_accuracy * 100))
---
+++ SAE/cub_demo.py
@@ -0,0 +1,43 @@
+"""
+Following code shows a demo for aPY dataset to reproduce the result of the paper:
+
+Semantic Autoencoder for Zero-shot Learning.
+
+Elyor Kodirov, Tao Xiang, and Shaogang Gong, CVPR 2017.
+
+Code originally written in Matlab and transformed to Python by Damares Resende
+"""
+
+import numpy as np
+from scipy.io import loadmat
+from utils import ZSL, Params
+from numpy.linalg import matrix_power
+
+cub = loadmat('../../Datasets/SAE/cub_demo_data.mat')
+
+# Dimension reduction
+X_tr = cub['X_tr']
+M, N = X_tr.transpose().dot(X_tr).shape
+Y = ZSL.label_matrix(cub['train_labels_cub'].transpose()).transpose()
+W = matrix_power(X_tr.transpose().dot(X_tr) + 50 * np.eye(M, N), -1).dot(X_tr.transpose()).dot(Y)
+
+X_tr = X_tr.dot(W)
+X_te = cub['X_te'].dot(W)
+
+lambda_ = .2
+param = Params(1, cub, 'cub')
+
+# Learn projection
+S_tr = ZSL.normalize_fea(np.array(cub['S_tr']))
+W = ZSL.SAE(X_tr.transpose(), S_tr.transpose(), lambda_).transpose()
+
+# [F --> S], projecting data from feature space to semantic space
+S_te_est = X_te.dot(W)
+zsl_accuracy, _ = ZSL.zsl_el(S_te_est, cub['S_te_pro'], param, 'cub')
+print('\n[1] AwA ZSL accuracy [V >>> S]: %.1f%%\n' % (zsl_accuracy * 100))
+
+# [S --> F], projecting from semantic to visual space
+X_te_pro = cub['S_te_pro'].dot(W.transpose())
+zsl_accuracy, _ = ZSL.zsl_el(X_te, X_te_pro, param, 'cub')
+
+print('[2] AwA ZSL accuracy [S >>> V]: %.1f%%\n' % (zsl_accuracy * 100))
