1e78f83f2afb44c880246afcb9c7e16e446a86aa
damaresende <dc.oliveiraresende@gmail.com>
2020-04-26 10:46:48 -0300

    Refactoring test_utils.py. Adding classmethod

--- baseline/sae/test/test_utils.py
+++ baseline/sae/test/test_utils.py
@@ -18,6 +18,14 @@ from baseline.sae.src.utils import ZSL
 
 
 class ZSLTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Initializes variables to be used in the tests
+        """
+        cls.data = loadmat('mockfiles/cub_demo_data.mat')
+        cls.labels = list(map(int, loadmat('mockfiles/cub_demo_data.mat')['train_labels_cub']))
+
     def test_sub2ind(self):
         """
         Tests if the returned value is equal to indexed position of rows and columns combinations
@@ -32,19 +40,17 @@ class ZSLTests(unittest.TestCase):
         indexes correspond to the index of the latest occurrence of a label
         """
         ref = loadmat('mockfiles/indexes.mat')['indexes'][0]
-        data = list(map(int, loadmat('mockfiles/cub_demo_data.mat')['train_labels_cub']))
-        indexes, labels = ZSL.is_member(data)
+        indexes, labels = ZSL.is_member(self.labels)
 
         self.assertTrue((list(map(int, ref - 1)) == indexes).all())
-        self.assertTrue((list(set(data)) == labels).all())
+        self.assertTrue((list(set(self.labels)) == labels).all())
 
     def test_label_matrix(self):
         """
         Tests if the matrix of labels returned matches all the expected values
         """
         ref = loadmat('mockfiles/label_matrix.mat')['label_matrix']
-        data = list(map(int, loadmat('mockfiles/cub_demo_data.mat')['train_labels_cub']))
-        mat = ZSL.label_matrix(data)
+        mat = ZSL.label_matrix(self.labels)
 
         self.assertTrue((mat == ref).all())
 
@@ -52,9 +58,7 @@ class ZSLTests(unittest.TestCase):
         """
         Tests if the x_tr and x_te returned matches all the expected values
         """
-        data = loadmat('mockfiles/cub_demo_data.mat')
-        labels = list(map(int, data['train_labels_cub']))
-        x_tr, x_te = ZSL.dimension_reduction(data['X_tr'], data['X_te'], labels)
+        x_tr, x_te = ZSL.dimension_reduction(self.data['X_tr'], self.data['X_te'], self.labels)
         ref = loadmat('mockfiles/dimension_reduction.mat')['dimension_reduction']
 
         self.assertTrue((np.around(x_tr, decimals=5) == np.around(ref['X_tr'][0][0], decimals=5)).all())
@@ -64,10 +68,8 @@ class ZSLTests(unittest.TestCase):
         """
         Tests if the matrix returned by the sylvester calculation matches all the expected values
         """
-        data = loadmat('mockfiles/cub_demo_data.mat')
-        labels = list(map(int, data['train_labels_cub']))
-        s_tr = normalize(data['S_tr'], norm='l2', axis=1, copy=False)
-        x_tr, _ = ZSL.dimension_reduction(data['X_tr'], data['X_te'], labels)
+        s_tr = normalize(self.data['S_tr'], norm='l2', axis=1, copy=False)
+        x_tr, _ = ZSL.dimension_reduction(self.data['X_tr'], self.data['X_te'], self.labels)
         ref = loadmat('mockfiles/sae.mat')['sae']
         w = ZSL.sae(x_tr.transpose(), s_tr.transpose(), .2).transpose()
 
@@ -78,15 +80,13 @@ class ZSLTests(unittest.TestCase):
         Tests if accuracy of Zero Shot Learning computed is the same as the expected one and if
         the returned classes of 1NN is the same as the reference
         """
-        data = loadmat('mockfiles/cub_demo_data.mat')
-        labels = list(map(int, data['train_labels_cub']))
-        s_tr = normalize(data['S_tr'], norm='l2', axis=1, copy=False)
-        x_tr, x_te = ZSL.dimension_reduction(data['X_tr'], data['X_te'], labels)
+        s_tr = normalize(self.data['S_tr'], norm='l2', axis=1, copy=False)
+        x_tr, x_te = ZSL.dimension_reduction(self.data['X_tr'], self.data['X_te'], self.labels)
         w = ZSL.sae(x_tr.transpose(), s_tr.transpose(), .2).transpose()
-        temp_labels = np.array([int(x) for x in data['te_cl_id']])
-        test_labels = np.array([int(x) for x in data['test_labels_cub']])
+        temp_labels = np.array([int(x) for x in self.data['te_cl_id']])
+        test_labels = np.array([int(x) for x in self.data['test_labels_cub']])
         ref = loadmat('mockfiles/y_hit.mat')['Y_hit5']
 
-        acc, y_hit = ZSL.zsl_el(x_te.dot(w), data['S_te_pro'], test_labels, temp_labels, 1, z_score=True)
+        acc, y_hit = ZSL.zsl_el(x_te.dot(w), self.data['S_te_pro'], test_labels, temp_labels, 1, z_score=True)
         self.assertEqual(0.61405, np.around(acc, decimals=5))
         self.assertTrue((ref == y_hit).all())
