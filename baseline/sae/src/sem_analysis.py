f0fdd31f168251fcc5b382a0e336438c8585357a
damaresende <dc.oliveiraresende@gmail.com>
2020-05-03 17:35:31 -0300

    Adding random option to sem_analysis.py

--- baseline/sae/src/sem_analysis.py
+++ baseline/sae/src/sem_analysis.py
@@ -38,14 +38,22 @@ class SemanticDegradation:
 
         self.n_folds = n_folds
         self.verbose = verbose
-        self.new_value = new_value
         self.rates = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+
+        if isinstance(new_value, int):
+            self.limits = None
+            self.new_value = new_value
+        elif isinstance(new_value, str) and new_value == 'random':
+            self.new_value = None
+            self.limits = (np.min(self.data.data['S_tr']), np.max(self.data.data['S_tr']))
+        else:
+            raise ValueError('Unexpected value for new_value. Please choose a real number or the string "random"')
 
         self.acc = {key: {'acc': np.zeros(self.n_folds), 'mean': 0, 'std': 0, 'max': 0, 'min': 0} for key in self.rates}
 
         self.data.set_semantic_data()
         self.acc['ref'] = self.data.v2s_projection()
-        self.acc['new_value'] = self.new_value
+        self.acc['new_value'] = new_value
 
         if self.verbose:
             self.print_degradation_status(0)
@@ -75,7 +83,7 @@ class SemanticDegradation:
         for rate in self.rates:
             for j in range(self.n_folds):
                 self.data.reset_weights()
-                self.data.set_semantic_data(self.kill_semantic_attributes(self.data.data['S_tr'], rate, self.new_value))
+                self.data.set_semantic_data(self.kill_semantic_attributes(self.data.data['S_tr'], rate))
 
                 if self.verbose:
                     logger.info('Computing fold %d...', j)
@@ -100,14 +108,12 @@ class SemanticDegradation:
         with open(filename, 'w+') as f:
             json.dump(json_string, f)
 
-    @staticmethod
-    def kill_semantic_attributes(data, rate, new_value):
+    def kill_semantic_attributes(self, data, rate):
         """
         Randomly sets to new_value a specific rate of the semantic attributes
 
         @param data: 2D numpy array with semantic data
         @param rate: float number from 0 to 1 specifying the rate of values to be replaced
-        @param new_value: float value that will replace the degraded ones
         @return: 2D numpy array with new data set
         """
         num_sem_attrs = data.shape[1]
@@ -118,7 +124,13 @@ class SemanticDegradation:
             for idx in random.sample(range(data.shape[1]), round(num_sem_attrs * rate)):
                 mask[idx] = True
 
-            new_data[ex, mask] = new_data[ex, mask] * 0 + new_value
+            if self.limits is None and self.new_value is not None:
+                new_data[ex, mask] = new_data[ex, mask] * 0 + self.new_value
+            else:
+                for i in range(len(mask)):
+                    if mask[i]:
+                        new_data[ex, i] = random.uniform(self.limits[0], self.limits[1])
+
         return new_data
 
 
@@ -128,7 +140,7 @@ if __name__ == '__main__':
     logging.basicConfig(filename=FILENAME, format=FORMAT, filemode='a', level=logging.DEBUG)
     logger = logging.getLogger('SemanticDegradation')
 
-    for key_value in [0, 0.001, 0.1, -1, 9999]:
+    for key_value in ['random', 0, 0.001, 0.1, -1, 9999]:
         logger.info('Key value is set to %s', str(key_value))
 
         logger.info('Evaluating AWA')
