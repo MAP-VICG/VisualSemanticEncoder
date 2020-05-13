0e924a14d4f60597879562d05994bb66fe082b55
damaresende <dc.oliveiraresende@gmail.com>
2020-05-13 05:06:30 -0300

    Refactoring sem_analysis.py

--- baseline/sae/src/sem_analysis.py
+++ baseline/sae/src/sem_analysis.py
@@ -1,120 +1,67 @@
-"""
-Runs SAE for different sets of semantic data. These data is modified according to a rate
-and a value to set. Rates from 10% to 100% are tested. As well as well as different values
-such as 0, 0.001, 0.1, -1 and 9999.
-
-@author: Damares Resende
-@contact: damaresresende@usp.br
-@since: Apr 27, 2020
-
-@organization: University of Sao Paulo (USP)
-    Institute of Mathematics and Computer Science (ICMC)
-    Laboratory of Visualization, Imaging and Computer Graphics (VICG)
-"""
 import json
 import random
-import logging
 import numpy as np
+from scipy.io import loadmat
+from sklearn.preprocessing import normalize
 
-from baseline.sae.src.awa_demo import AWA
-from baseline.sae.src.cub_demo import CUB200
+from baseline.sae.src.utils import ZSL
 
 
 class SemanticDegradation:
-    def __init__(self, datafile, n_folds, new_value, data_type, verbose=False):
+    def __init__(self, datafile, data_type, new_value=None):
         """
-        Initializes auxiliary variables.
 
-        :param datafile: .mat file with data
-        :param n_folds: number of folds to degrade data per rate
-        :param new_value: value to replace the original one
-        :param data_type: type of data to be analysed ("cub" or "awa")
-        :param verbose: if true, activates printing messages to log file
+        :param data_type:
+        :param new_value: real value to replace to. If not specified, a random value will be chosen
         """
+        self.data_type = data_type
+        self.new_value = new_value
+        self.data = loadmat(datafile)
+
         if data_type == 'awa':
-            self.data = AWA(datafile)
+            self.z_score = False
+            self.temp_labels = np.array([int(x) for x in self.data['param']['testclasses_id'][0][0]])
+            self.test_labels = np.array([int(x) for x in self.data['param']['test_labels'][0][0]])
+            self.s_te_pro = normalize(self.data['S_te_pro'].transpose(), norm='l2', axis=1, copy=True).transpose()
         elif data_type == 'cub':
-            self.data = CUB200(datafile)
+            self.z_score = True
+            self.temp_labels = np.array([int(x) for x in self.data['te_cl_id']])
+            self.test_labels = np.array([int(x) for x in self.data['test_labels_cub']])
+            self.s_te_pro = self.data['S_te_pro']
 
-        self.n_folds = n_folds
-        self.verbose = verbose
-        self.rates = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+            labels = list(map(int, self.data['train_labels_cub']))
+            self.data['X_tr'], self.data['X_te'] = ZSL.dimension_reduction(self.data['X_tr'], self.data['X_te'], labels)
 
-        if isinstance(new_value, int):
-            self.limits = None
-            self.new_value = new_value
-        elif isinstance(new_value, str) and new_value == 'random':
-            self.new_value = None
-            self.limits = (np.min(self.data.data['S_tr']), np.max(self.data.data['S_tr']))
+        if new_value is None:
+            self.limits = (np.min(self.data['S_tr']), np.max(self.data['S_tr']))
+
+    def estimate_semantic_data(self, vis_tr_data, sem_tr_data, vis_te_data):
+        """
+        Trains SAE and applies it to visual data in order to estimate its correspondent semantic data
+
+        :param vis_tr_data: visual training data
+        :param sem_tr_data: semantic training data
+        :param vis_te_data: visual test data
+        :return: 2D numpy array with estimated semantic data
+        """
+        if self.data_type == 'awa':
+            x_tr = normalize(vis_tr_data.transpose(), norm='l2', axis=1, copy=True).transpose()
+            w = ZSL.sae(x_tr.transpose(), sem_tr_data.transpose(), 500000)
+            return vis_te_data.dot(normalize(w, norm='l2', axis=1, copy=True).transpose())
+        elif self.data_type == 'cub':
+            s_tr = normalize(sem_tr_data, norm='l2', axis=1, copy=False)
+            w = ZSL.sae(vis_tr_data.transpose(), s_tr.transpose(), .2).transpose()
+            return vis_te_data.dot(w)
         else:
-            raise ValueError('Unexpected value for new_value. Please choose a real number or the string "random"')
-
-        self.acc = {key: {'acc': np.zeros(self.n_folds), 'mean': 0, 'std': 0, 'max': 0, 'min': 0} for key in self.rates}
-
-        self.data.set_semantic_data()
-        self.acc['ref'] = self.data.v2s_projection()
-        self.acc['new_value'] = new_value
-
-        if self.verbose:
-            self.print_degradation_status(0)
-
-    def print_degradation_status(self, rate):
-        """
-        Prints the number of values that could be modified in the semantic array.
-
-        :param rate: rate expected in degradation
-        :return: None
-        """
-        counts = [0] * self.data.s_tr.shape[0]
-        for i in range(self.data.s_tr.shape[0]):
-            for j in range(self.data.s_tr.shape[1]):
-                if self.data.s_tr[i][j] == self.new_value:
-                    counts[i] += 1
-        logger.info('Number of degraded values is %s for rate %.2f', str(set(counts)), rate)
-
-    def degrade_semantic_data(self):
-        """
-        Randomly replaces the values of the semantic array for a new value specified and runs SAE over it.
-        Saves the resultant accuracy in a dictionary. Data is degraded with rates ranging from 10 to 100%
-        for a specific number of folds.
-
-        :return: None
-        """
-        for rate in self.rates:
-            for j in range(self.n_folds):
-                self.data.reset_weights()
-                self.data.set_semantic_data(self.kill_semantic_attributes(self.data.data['S_tr'], rate))
-
-                if self.verbose:
-                    logger.info('Computing fold %d...', j)
-                    self.print_degradation_status(rate)
-
-                self.acc[rate]['acc'][j] = self.data.v2s_projection()
-
-            self.acc[rate]['mean'] = np.mean(self.acc[rate]['acc'])
-            self.acc[rate]['std'] = np.std(self.acc[rate]['acc'])
-            self.acc[rate]['max'] = np.max(self.acc[rate]['acc'])
-            self.acc[rate]['min'] = np.min(self.acc[rate]['acc'])
-            self.acc[rate]['acc'] = ', '.join(list(map(str, self.acc[rate]['acc'])))
-
-    def write2json(self, filename):
-        """
-        Writes data from accuracy dictionary to JSON file
-
-        :param filename: string with name of file to write data to
-        :return: None
-        """
-        json_string = json.dumps(self.acc)
-        with open(filename, 'w+') as f:
-            json.dump(json_string, f)
+            raise ValueError('Unknown type of data')
 
     def kill_semantic_attributes(self, data, rate):
         """
         Randomly sets to new_value a specific rate of the semantic attributes
 
-        @param data: 2D numpy array with semantic data
-        @param rate: float number from 0 to 1 specifying the rate of values to be replaced
-        @return: 2D numpy array with new data set
+        :param data: 2D numpy array with semantic data
+        :param rate: float number from 0 to 1 specifying the rate of values to be replaced
+        :return: 2D numpy array with new data set
         """
         num_sem_attrs = data.shape[1]
 
@@ -133,22 +80,50 @@ class SemanticDegradation:
 
         return new_data
 
+    def degrade_semantic_data(self, n_folds):
+        """
+        Randomly replaces the values of the semantic array for a new value specified and runs SAE over it.
+        Saves the resultant accuracy in a dictionary. Data is degraded with rates ranging from 10 to 100%
+        for a specific number of folds.
+
+        :return: None
+        """
+        rates = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+        acc_dict = {key: {'acc': np.zeros(n_folds), 'mean': 0, 'std': 0, 'max': 0, 'min': 0} for key in rates}
+
+        for rate in rates:
+            for j in range(n_folds):
+                s_tr = self.kill_semantic_attributes(self.data['S_tr'], rate)
+                s_te = self.estimate_semantic_data(self.data['X_tr'], s_tr, self.data['X_te'])
+
+                acc, _ = ZSL.zsl_el(s_te, self.s_te_pro, self.test_labels, self.temp_labels, 1, self.z_score)
+                acc_dict[rate]['acc'][j] = acc
+
+            acc_dict[rate]['mean'] = np.mean(acc_dict[rate]['acc'])
+            acc_dict[rate]['std'] = np.std(acc_dict[rate]['acc'])
+            acc_dict[rate]['max'] = np.max(acc_dict[rate]['acc'])
+            acc_dict[rate]['min'] = np.min(acc_dict[rate]['acc'])
+            acc_dict[rate]['acc'] = ', '.join(list(map(str, acc_dict[rate]['acc'])))
+
+        return acc_dict
+
+    @staticmethod
+    def write2json(acc_dict, filename):
+        """
+        Writes data from accuracy dictionary to JSON file
+
+        :param acc_dict: dict with classification accuracies
+        :param filename: string with name of file to write data to
+        :return: None
+        """
+        json_string = json.dumps(acc_dict)
+        with open(filename, 'w+') as f:
+            json.dump(json_string, f)
+
 
 if __name__ == '__main__':
-    FORMAT = '%(asctime)-15s %(message)s'
-    FILENAME = '../../../plotter/data/semantic_degradation.log'
-    logging.basicConfig(filename=FILENAME, format=FORMAT, filemode='a', level=logging.DEBUG)
-    logger = logging.getLogger('SemanticDegradation')
+    sem = SemanticDegradation('../../../../Datasets/SAE/awa_demo_data.mat', 'awa')
+    sem.write2json(sem.degrade_semantic_data(n_folds=10), '../../../plotter/data/awa_v2s_projection_random.json')
 
-    for key_value in ['random', 0, 0.001, 0.1, -1, 9999]:
-        logger.info('Key value is set to %s', str(key_value))
-
-        logger.info('Evaluating AWA')
-        sem = SemanticDegradation('../../../../Datasets/SAE/awa_demo_data.mat', 10, key_value, 'awa', verbose=True)
-        sem.degrade_semantic_data()
-        sem.write2json('../../../plotter/data/awa_v2s_projection_%s.json' % str(key_value))
-
-        logger.info('Evaluating CUB200')
-        sem = SemanticDegradation('../../../../Datasets/SAE/cub_demo_data.mat', 10, key_value, 'cub', verbose=True)
-        sem.degrade_semantic_data()
-        sem.write2json('../../../plotter/data/cub_v2s_projection_%s.json' % str(key_value))
+    sem = SemanticDegradation('../../../../Datasets/SAE/cub_demo_data.mat', 'cub')
+    sem.write2json(sem.degrade_semantic_data(n_folds=10), '../../../plotter/data/cub_v2s_projection_random.json')
