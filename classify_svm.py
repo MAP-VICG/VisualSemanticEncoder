3270070559bf1c79550d616532815f4990150621
damaresende <dc.oliveiraresende@gmail.com>
2020-08-30 21:56:15 -0300

    Adding logging messages

--- classify_svm.py
+++ classify_svm.py
@@ -1,5 +1,6 @@
 import os
 import json
+import logging
 
 from encoders.sec.src.encoder import ModelType
 from encoders.tools.src.svm_classification import SVMClassifier, DataType
@@ -21,6 +22,7 @@ class Classification:
         if self.save and not os.path.isdir(results_path):
             os.makedirs(results_path)
 
+        logging.info('Classifying dataset %s' % label)
         svm = SVMClassifier(data_type, self.model_type, self.folds, self.epochs, degradation_rate=rate)
         vis_data, lbs_data, sem_data = svm.get_data(data_path)
 
@@ -32,18 +34,24 @@ class Classification:
         self.result[label]['sec'] = svm.classify_sec_data(vis_data, sem_data, lbs_data, self.save, results_path)
         self.result[label]['s2s'] = svm.classify_sae2sec_data(vis_data, sem_data, lbs_data, self.save, results_path)
 
-    def classify_all(self, rate):
         rate_label = str(int(rate * 100))
+        with open(os.path.join(self.results_path, 'classification_results_%s.json' % rate_label.zfill(3)), 'w+') as f:
+            json.dump(self.result, f, indent=4, sort_keys=True)
+
+    def classify_all(self, rate):
+        logging.info('Running classification for degradation rate of %.2f' % rate)
         self.run_classification('../Datasets/SEM/cub_demo_data.mat', 'i_cub', DataType.CUB, rate=rate)
         self.run_classification('../Datasets/SEM/awa_demo_data.mat', 'i_awa', DataType.AWA, rate=rate)
         self.run_classification('../Datasets/SEM/cub_demo_data_resnet.mat', 'r_cub', DataType.CUB, rate=rate)
         self.run_classification('../Datasets/SEM/awa_demo_data_resnet.mat', 'r_awa', DataType.AWA, rate=rate)
 
-        with open(os.path.join(self.results_path, 'classification_results_%s.json' % rate_label.zfill(3)), 'w+') as f:
-            json.dump(self.result, f, indent=4, sort_keys=True)
-
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        filename='classify_svm.log',
+                        format='%(asctime)s.%(msecs)03d %(levelname)s [%(module)s, %(funcName)s]: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
     for degradation_rate in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
         klass = Classification(5, 50, 'results_1l', ModelType.SIMPLE_AE_1L)
         klass.classify_all(degradation_rate)
