e6ea0cc9cd208fadba14d5fd053335dc59d0c302
damaresende <dc.oliveiraresende@gmail.com>
2020-09-13 11:10:07 -0300

    Refactoring plots

--- plotter/accuracy_analysis.py
+++ plotter/accuracy_analysis.py
@@ -1,23 +1,15 @@
+import os
 import json
 import numpy as np
 import matplotlib.pyplot as plt
 
 dataset_names = {'i_awa': 'AwA GoogleNet', 'r_awa': 'AwA2 ResNet', 'i_cub': 'CUB GoogleNet', 'r_cub': 'CUB ResNet'}
-result_files = ['../results/accuracy/classification_results_000.json',
-                '../results/accuracy/classification_results_010.json',
-                '../results/accuracy/classification_results_020.json',
-                '../results/accuracy/classification_results_030.json',
-                '../results/accuracy/classification_results_040.json',
-                '../results/accuracy/classification_results_050.json',
-                '../results/accuracy/classification_results_060.json',
-                '../results/accuracy/classification_results_070.json',
-                '../results/accuracy/classification_results_080.json',
-                '../results/accuracy/classification_results_090.json',
-                '../results/accuracy/classification_results_100.json']
+result_files = sorted(['../results/accuracy/' + file for file in os.listdir('../results/accuracy') if file.endswith('.json')])
 
 
 def plot_classification_results():
-    plt.figure(figsize=(11, 6.5))
+    ax = None
+    fig = plt.figure(figsize=(11, 6.5))
     for p, dataset in enumerate(['i_awa', 'r_awa', 'i_cub', 'r_cub']):
         rates = []
         k = len(result_files)
@@ -43,7 +35,10 @@ def plot_classification_results():
             curves['vis']['mean'][i], curves['vis']['std'][i] = np.mean(data[dataset]['vis']), np.std(data[dataset]['vis'])
             curves['pca']['mean'][i], curves['pca']['std'][i] = np.mean(data[dataset]['pca']), np.std(data[dataset]['pca'])
 
-        plt.subplot(2, 2, p + 1)
+        ax = plt.subplot(2, 2, p + 1)
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 - 0.1, box.width, box.height])
+
         for key in curves.keys():
             if key == 'vis':
                 plt.errorbar(rates, curves[key]['mean'], yerr=curves[key]['std'], label=key, ls='-.', color='#C0C0C0')
@@ -52,21 +47,20 @@ def plot_classification_results():
             else:
                 plt.errorbar(rates, curves[key]['mean'], yerr=curves[key]['std'], label=key)
 
-        if dataset in ['i_awa', 'r_awa']:
-            plt.legend(loc='lower left')
-        else:
-            plt.legend(loc='upper right')
-
         plt.ylabel('Accuracy', weight='bold')
         plt.xlabel('Degradation Rate (%)', weight='bold')
         plt.title('SVM - %s' % dataset_names[dataset], weight='bold')
         plt.tight_layout()
 
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=7, bbox_to_anchor=(0.5, 0.05))
+
     plt.show()
 
 
 def plot_v1_vs_v2():
-    plt.figure(figsize=(11, 6.5))
+    ax = None
+    fig = plt.figure(figsize=(11, 6.5))
     for p, dataset in enumerate(['i_awa', 'r_awa', 'i_cub', 'r_cub']):
         rates = []
         k = len(result_files)
@@ -87,20 +81,20 @@ def plot_v1_vs_v2():
             curves['sec_v1']['mean'][i], curves['sec_v1']['std'][i] = np.mean(data[dataset]['sec_v1']), np.std(data[dataset]['sec_v1'])
             curves['sec_v2']['mean'][i], curves['sec_v2']['std'][i] = np.mean(data[dataset]['sec_v2']), np.std(data[dataset]['sec_v2'])
 
-        plt.subplot(2, 2, p + 1)
+        ax = plt.subplot(2, 2, p + 1)
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 - 0.1, box.width, box.height])
+
         for key in curves.keys():
             plt.errorbar(rates, curves[key]['mean'], yerr=curves[key]['std'], label=key)
-
-        if dataset in ['i_awa', 'r_awa']:
-            plt.legend(loc='lower left')
-        else:
-            plt.legend(loc='upper right')
 
         plt.ylabel('Accuracy', weight='bold')
         plt.xlabel('Degradation Rate (%)', weight='bold')
         plt.title('SVM - %s' % dataset_names[dataset], weight='bold')
         plt.tight_layout()
 
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=4, bbox_to_anchor=(0.5, 0.05))
     plt.show()
 
 
