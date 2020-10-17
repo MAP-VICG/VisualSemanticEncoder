f96441e552ca5c1eaba76b775646e32080e1c8d7
damaresende <dc.oliveiraresende@gmail.com>
2020-10-17 12:08:56 -0300

    Saving SUN features into mat file

---
+++ featureextraction/src/vocsunparser.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.io import loadmat, savemat
+
+with open('../../../../Desktop/SUN_x_train_sem.txt') as f:
+    sun_sem = np.array([list(map(float, line.split())) for line in f.readlines()])
+
+with open('../../../../Desktop/SUN_x_train_vis.txt') as f:
+    sun_vis = np.array([list(map(float, line.split())) for line in f.readlines()])
+
+with open('../../../../Desktop/SUN_y_train.txt') as f:
+    sun_labels = np.array([list(map(float, line.split())) for line in f.readlines()])
+
+data = loadmat('../../../../Downloads/SUNAttributeDB/images.mat')
+labels = [image[0][0].split('/')[1] for image in data['images']]
+
+data_structure = dict()
+data_structure['S_tr'] = sun_sem
+data_structure['X_tr'] = sun_vis
+data_structure['S_te_pro'] = sun_sem
+data_structure['tr_cl_id'] = list(set([int(lb[0]) for lb in sun_labels]))
+data_structure['train_labels'] = np.array([[int(lb[0])] for lb in sun_labels])
+data_structure['labels_dict'] = {label.strip(): i + 1 for i, label in enumerate(set(labels))}
+data_structure['X_te'] = data_structure['S_tr'] = data_structure['test_labels'] = data_structure['te_cl_id'] = 'none'
+
+savemat('../../../Datasets/sun_data_resnet50.mat', data_structure)
